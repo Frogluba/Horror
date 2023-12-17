97e54073b808e2ced14db037f279ce322b725438
7700s <146203655+Frogluba@users.noreply.github.com>
2023-12-17 14:03:36 +0200

    somethingmade

---
+++ Assets/Scripts/FlashLite.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLite : MonoBehaviour
+{
+  public bool isOn;
+  public Light light;
+  public AudioClip click;
+  public int hp;
+
+    private void Update()
+    {
+        light.enabled = isOn;
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Switch();
+        }
+
+        if(isOn = true)
+        {
+            Invoke("Hp", 10f);
+        }
+    }
+    void Hp()
+    {
+        hp -= 1;
+    }
+
+    void Switch()
+    {
+        isOn = !isOn;
+        var source = GetComponent<AudioSource>();
+        source.clip = click;
+        source.Play();
+    }
+}
